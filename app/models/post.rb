d1feafffb15acbd7c2479fee807300bfc4507ec8
philip fraser <pr.fraser3@gmail.com>
2017-09-28 15:41:45 +1000

    added hashtags

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
 	has_attached_file :avatar, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  acts_as_taggable_on :tags
+
 	belongs_to :user
 end
